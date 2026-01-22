abc70bd46ac0d147a7bce8b0edb1b5573b42bb9f
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-22 22:00:30 +0530

    made changes

---
+++ script.js
@@ -0,0 +1,17 @@
+let turn = true;
+
+let button = document.querySelectorAll('.button');
+
+button.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (turn)
+        {
+            btn.textContent = 'X';
+        }
+        else
+        {
+            btn.textContent = 'O';
+        }
+    })
+}
+)
